89def73cb94b100734bda44e88fcc67e7a18dd88
Trent Ford <trentford@Trents-MacBook-Air.local>
2015-11-03 12:24:27 -0600

    Adding the initial code files
    Adding all of the initial code, text, and gradle files

---
+++ SuperFightGame/src/main/java/game/SuperFight.java
@@ -0,0 +1,57 @@
+package game;
+
+import java.util.*;
+
+public class SuperFight {
+//to avoid confusion, I will refer to Characters in the game as gameCharacters to avoid confusion with java character
+
+
+//**********************************************************************
+//functions
+
+
+//read in AttributeCards.txt and 
+//place items in GameAttributes String[]
+//385 lines, each line is one item
+
+//read in CharacterCards.txt and 
+//place items in gameCharacters String[]
+//229 lines, each line is one item
+
+//pick 3 random, non-same items from GameCharacters
+//save to a temporary gameCharacter string array of 3 strings
+
+//pick 5 random, non-same items from GameAttributes
+//save to a temporary attribute string array of 5 strings
+
+
+
+
+//**********************************************************************
+//displaying
+
+//display the temporary gameCharacters array and temporary attribute array
+
+
+
+//**********************************************************************
+
+
+}
+
+/*
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
+*/
+
+
+
+
+
+
+
+
---
+++ SuperFightGame/src/test/java/game/SuperFightTest.java
@@ -0,0 +1,35 @@
+package game;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SuperFightTest {
+    
+    
+    @Test
+    public void testThatGradleIsReadingCorrectly() {
+        assertTrue(true);
+    }
+    
+    
+    @Test
+    public void testThatAttributesTextFileIsReadIn() {
+	    assertTrue(false);
+    }
+
+    @Test
+    public void testThatCharactersTextFileIsReadIn() {
+	    assertTrue(false);
+    }   
+    
+    @Test
+    public void testThatSpecificCharacterCanBeReadFromArray() {
+	    assertTrue(false);
+    } 
+    
+     @Test
+    public void testThatSpecificAttributeCanBeReadFromArray() {
+	    assertTrue(false);
+    } 
+    
+}
